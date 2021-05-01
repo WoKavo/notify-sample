c7c1fe0427e60c66e46f8a3f907dba3c174589b7
wokavo <->
2021-05-01 21:33:43 +0300

    src

---
+++ NotifySample/IconConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace NotifySample
+{
+	public class IconConverter : IMultiValueConverter
+	{
+		private BitmapImage high;
+		private BitmapImage High => high ??= App.Current.FindResource("High") as BitmapImage;
+
+		private BitmapImage mid;
+		private BitmapImage Mid => mid ??= App.Current.FindResource("Mid") as BitmapImage;
+
+		private BitmapImage low;
+		private BitmapImage Low => low ??= App.Current.FindResource("Low") as BitmapImage;
+
+		private BitmapImage mute;
+		private BitmapImage Mute => mute ??= App.Current.FindResource("Mute") as BitmapImage;
+
+		private BitmapImage noDev;
+		private BitmapImage NoDevice => noDev ??= App.Current.FindResource("NoDevice") as BitmapImage;
+
+		private BitmapImage currentIcon;
+		
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			BitmapImage newIcon = null;
+			if(values[0] is true) //no devices
+			{
+				newIcon = NoDevice;
+			}
+			else
+			{
+				if(values[2] is true) //is muted
+					newIcon = Mute;
+				else if(values[1] is int newVol)
+					newIcon = newVol > 65 ? High : (newVol < 35) ? Low : Mid;
+			}
+			if(newIcon == null || this.currentIcon == newIcon)
+				return Binding.DoNothing;
+			this.currentIcon = newIcon;
+			return newIcon;
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => null;
+	}
+}
---
+++ NotifySample/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors.Core;
+
+namespace NotifySample
+{
+	public partial class MainWindow : INotifyPropertyChanged
+	{
+		private int volume = 50;
+		public int Volume
+		{
+			get => this.volume;
+			set
+			{
+				this.volume = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private bool isMuted;
+		public bool IsMuted
+		{
+			get => this.isMuted;
+			set
+			{
+				this.isMuted = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private bool noDevice;
+		public bool NoDevice
+		{
+			get => this.noDevice;
+			set
+			{
+				this.noDevice = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private ICommand muteCmd;
+		public ICommand MuteCommand
+		{
+			get => this.muteCmd;
+			set
+			{
+				this.muteCmd = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private ICommand noDeviceCmd;
+		public ICommand NoDeviceCommand
+		{
+			get => this.noDeviceCmd;
+			set
+			{
+				this.noDeviceCmd = value;
+				OnPropertyChanged();
+			}
+		}
+		
+		public MainWindow()
+		{
+			InitializeComponent();
+			this.MuteCommand = new ActionCommand(() => this.IsMuted = !this.IsMuted);
+			this.NoDeviceCommand = new ActionCommand(() => this.NoDevice = !this.NoDevice);
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
